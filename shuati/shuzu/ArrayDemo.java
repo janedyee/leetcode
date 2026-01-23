6e3dc4da76e753812b77dad1d54d2529d3993a0b
your name <your_email@youremail.com>
2026-01-23 21:39:25 +0800

    1.23数组和链表学习

---
+++ shuati/shuzu/ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[0] = 1;
+        arr[1] = 2;
+        
+        int a = arr[1];
+        System.out.print(a);
+    }
+}
